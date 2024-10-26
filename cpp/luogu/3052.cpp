d0eb079b616629ad0d56adbdb84b5f81946fe73a
kzzz-jpg <211096@stu.cchs.chc.edu.tw>
2024-10-26 08:13:54 +0800

    2024/10/26

---
+++ cpp/luogu/3052.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+struct ev{
+	int ti,pe;
+	ev(int t=1e15,int p=1e15):ti(t),pe(p){}
+	bool operator< (ev o) const{
+		if(ti==o.ti) return pe<o.pe;
+		return ti<o.ti;
+	}
+} dp[1<<19];
+int w,a[20],n;
+signed main(){
+	cin>>n>>w;
+	for(int i=0;i<n;i++) cin>>a[i];
+	dp[0]=ev(1,0);
+	for(int i=0;i<(1<<n);i++){
+		for(int j=0;j<n;j++){
+			ev nw;
+			if(i&(1<<j)){
+				nw=dp[i&(~(1<<j))];
+				if(nw.pe+a[i]>w) nw.ti++,nw.pe=a[i];
+				else nw.pe+=a[i];
+			}
+			dp[i]=min(dp[i],nw);
+		}
+	}
+	cout<<dp[(1<<n)-1].ti<<'\n';
+}
