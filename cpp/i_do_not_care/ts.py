9af5c58e127f7ca5fa6cd0ad29b6e403655656b1
kzzz-jpg <211096@stu.cchs.chc.edu.tw>
2024-12-05 00:23:15 +0800

    2024/12/5

--- cpp/i_do_not_care/ts.py
+++ cpp/i_do_not_care/ts.py
@@ -1,12 +1,31 @@
-n,m=map(int,input().split())
-a=[int(_) for _ in input().split()]
-for i in range(1,n):
-    a[i]+=a[i-1]
-for i in range(m):
-    l,r=map(int,input().split())
-    l-=1
-    r-=1
-    if l == 0:
-        print(a[r]%10009)
-    else:
-        print((a[r]-a[l-1])%10009)
+while True:
+    try:
+        x=int(input())
+        y=list(str(input()))
+        z=int(input())
+        bb=0
+        cc=[]
+        for i in range(z):
+            aa=int(input())
+            try:
+                y[aa-1]='1'
+            except:
+                aa=1
+        for i in y:
+            if i=='1':
+                bb=bb+1
+            else:
+                if bb>0:
+                    cc.append(bb)
+                bb=0
+        if bb>0:
+            cc.append(bb)
+        gg=max(cc)
+        kk=min(cc)
+        #if kk==gg:
+        #    print(kk)
+        #else:
+        print(kk)
+        print(gg)
+    except EOFError:
+        break
